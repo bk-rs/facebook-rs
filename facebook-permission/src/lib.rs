fa05dc575c270aab8be2fc42030c48366e3a191d
vkill <vkill.net@gmail.com>
2022-11-25 15:12:13 +0800

    Tweak

--- facebook-permission/src/lib.rs
+++ facebook-permission/src/lib.rs
@@ -1,6 +1,6 @@
 use serde_enum_str::{Deserialize_enum_str, Serialize_enum_str};
 
-#[derive(Deserialize_enum_str, Serialize_enum_str, PartialEq, Eq, Hash, Debug, Clone)]
+#[derive(Deserialize_enum_str, Serialize_enum_str, Debug, Clone, PartialEq, Eq, Hash)]
 #[serde(rename_all = "snake_case")]
 pub enum FacebookPermission {
     // Don't Edit, copy from html_parser
